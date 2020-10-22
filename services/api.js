32115a127760cbb33378c8ed5495b6de93fdae24
ponga2112 <ponga@picturewell.com>
2020-10-22 11:23:24 -0700

    api functions completed

--- services/api.js
+++ services/api.js
@@ -6,47 +6,84 @@ const send = async (action,message) => {
     // send(): send the api request
     // send(): returns a tuple (bool,message) where bool indicated if request was successful or not and message contains the response body json, or error message
     let response = {};
-    if (action == "ping") {
-        response = await fetch(API_URL+'/'+action, {
-            method: 'GET',
-            mode: 'cors',
-            cache: 'no-cache',
-            redirect: 'error',
-            referrerPolicy: 'origin'
-        });
-    } else {
-        response = await fetch(API_URL+'/'+action, {
-            method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            headers: {'Content-Type':'application/json'},
-            redirect: 'error',
-            referrerPolicy: 'origin',
-            body: JSON.stringify(message)
-        });
+    let result = {
+        status: false,
+        body: {}
+    };
+    try {
+        if (action == "ping") {
+            response = await fetch(API_URL+'/'+action, {
+                method: 'GET',
+                mode: 'cors',
+                cache: 'no-cache',
+                redirect: 'error',
+                referrerPolicy: 'origin'
+            }).then(function(response){
+                if(response.status == 200) {
+                    result.status = true;
+                } else {
+                    result.status = false;
+                }
+                response.json().then(function(body){
+                    result.body = body;
+                })
+            });
+        } else {
+            response = await fetch(API_URL+'/'+action, {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                headers: {'Content-Type':'application/json'},
+                redirect: 'error',
+                referrerPolicy: 'origin',
+                body: JSON.stringify(message)
+            }).then(function(response){
+                if(response.status == 200) {
+                    result.status = true;
+                } else {
+                    result.status = false;
+                }
+                response.json().then(function(body){
+                    result.body = body;
+                })
+            });
+        }
+    } catch(e) {
+        result.body = {e};
     }
-    // TODO: this still needs work:
-    // ctf.api.ping().then(response => response.json()).then(data => console.log(data));
-    return response;
+    // send() returns a promise for the API call results object
+    return result;
 }
 
 const API = {
     isConnected: false, 
+    // Example usage for API:
+    //
+    // ctf.api.ping().then(function(response){
+    //    if(response.status) {
+    //        API.isConnected = true;
+    //    }
+    // });
+    // 
+    // ctf.api.create(name).then(function(response){
+    //    if(response.status) {
+    //        STATE.API.isConnected = true;
+    //        STATE.API.handle = response.body.handle;
+    //        STATE.API.guid = response.body.token;
+    //    }
+    // });
+    //
     ping : async () => {
         return send("ping",{});
     },
-    create : async () => {
-        // TODO: write this!
-
-        return false;
+    create : async (name) => {
+        return send("create",{token:ctf.state.API.guid, name:name});
     },
-    capture : async () => {
-        // TODO: write this!
-        return false;
+    capture : async (flag,points) => {
+        return send("capture",{token:ctf.state.API.guid, flag:flag, points:points});
     },
     leaders : async () => {
-        // TODO: write this!
-        return false;
+        return send("leaders",{token:ctf.state.API.guid});
     }
 }
 
