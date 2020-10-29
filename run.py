302ba4e35dbec22789585c53208b7adcedc5e641
ponga2112 <ponga@picturewell.com>
2020-10-29 11:45:38 -0700

    run.py bootstrap script

---
+++ run.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python3
+
+#
+## bootstrap to serve local files and run the api server
+#
+
+import http.server, socketserver, threading
+# TODO: this flask app is pointing to test, once API server is completed, change this line:
+from api import test as apiServer
+
+API_PORT = 8002
+HTTP_PORT = 8001
+
+def run():
+    try:
+        httpd = socketserver.TCPServer(('0.0.0.0', HTTP_PORT), http.server.SimpleHTTPRequestHandler)
+        httpd_thread = threading.Thread(target=httpd.serve_forever)
+        httpd_thread.daemon = True
+        httpd_thread.start()
+        print('[+] HTTP Server running on port '+str(HTTP_PORT)+"...")
+        apiServer.initDB()
+        apiServer.app.config['ENV'] = 'development'
+        apiServer.app.run(host='0.0.0.0',port=API_PORT)
+    except KeyboardInterrupt:
+        print('[+] Shutting down')
+        httpd_thread.stop()
+        exit(0)
+
+run()
+exit(0)
